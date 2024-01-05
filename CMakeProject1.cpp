6fced9cea9cb2ce8b7e123a2f76164bd637e7f0e
Pan Nous <chasi@gmx.de>
2024-01-04 23:41:08 -0800

    Add project files.

---
+++ CMakeProject1.cpp
@@ -0,0 +1,20 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+void fehler()
+{
+	cout << "Fehler" << endl;
+	//throw "Fehler";
+}
+void test() {
+	fehler();
+}
+int main()
+{
+	test();
+	cout << "Hello CMake.!!" << endl;
+	return 0;
+}
